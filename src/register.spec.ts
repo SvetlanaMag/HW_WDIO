e41b610dc64275c36819c58d5f9fe39b9aa61c48
SvetlanaMag <svetlanammmsv@gmail.com>
2024-07-29 23:00:51 +0300

    fix creds

--- src/register.spec.ts
+++ src/register.spec.ts
@@ -8,28 +8,62 @@ const passwordLoginSelector = 'input#password';
 const submitButtonSelector= 'input.button#submit';
 const messageSelector = 'h4#errorMessageOnRegister';
 const messageLoginSelector = 'h4#errorMessage';
-const validCredentials = {
-    username: 'MaryMM',
-    password: 'Password'
-};
-const actualSuccessMessageText = 'Successfully registered! Please, click Back to return on login page'
 
-const inValidCredentials = {
-    username: ['', 'Ma', ' Mary '],
-    password: ['', 'Passwor', 'password'],
-    errorMessageRegisterUsername: ['Username is required',
-                            'Username should contain at least 3 characters', 
-                            'Prefix and postfix spaces are not allowed is username'],
-    errorMessageRegisterPassword: ['Password is required',
-                            'Password should contain at least 8 characters', 
-                            'Password should contain at least 1 character in lower case'],
-    errorMessageLoginUsername: ['Username is required',
-                                'Invalid credentials', 
-                                'Invalid credentials'],
-    errorMessageLoginPassword: ['Password is required',
-                                'Invalid credentials', 
-                                'Invalid credentials']
-};
+const credsCheckUsernameField = [
+    {
+        checkName: 'valid username & valid password',
+        username: 'Mary May',
+        password: 'SecretPassword',
+        expectedNotification: 'Successfully registered! Please, click Back to return on login page',
+        expectedNotificationLogin: ''
+    },
+    {
+        checkName: 'empty username & valid password',
+        username: '',
+        password: 'SecretPassword',
+        expectedNotification: 'Username is required',
+        expectedNotificationLogin: 'Username is required'
+    },
+    {
+        checkName: 'username 2 characters & valid password',
+        username: 'Ma',
+        password: 'SecretPassword',
+        expectedNotification: 'Username should contain at least 3 characters',
+        expectedNotificationLogin: 'Invalid credentials'
+    },
+    {
+        checkName: 'username with prefix/postfix spaces & valid password',
+        username: ' Mary May ',
+        password: 'SecretPassword',
+        expectedNotification: 'Prefix and postfix spaces are not allowed is username',
+        expectedNotificationLogin: 'Invalid credentials'
+    },
+];
+
+const credsCheckPasswordField = [
+    {
+        checkName: 'empty username & valid password',
+        username: 'John Doe',
+        password: '',
+        expectedNotification: 'Password is required',
+        expectedNotificationLogin: 'Password is required'
+    },
+    {
+        checkName: 'username 2 characters & valid password',
+        username: 'John Doe',
+        password: 'SecretP',
+        expectedNotification: 'Password should contain at least 8 characters',
+        expectedNotificationLogin: 'Invalid credentials'
+    },
+    {
+        checkName: 'username with prefix/postfix spaces & valid password',
+        username: 'John Doe',
+        password: 'secretpassword',
+        expectedNotification: 'Password should contain at least 1 character in lower case',
+        expectedNotificationLogin: 'Invalid credentials'
+    },
+];
+ 
 
 describe('Task1', () => {
 
@@ -43,14 +77,14 @@ describe('Task1', () => {
         const usernameFieldLink = await $(usernameRegisterSelector);
         const passwordFieldLink = await $(passwordRegisterSelector);
         const register2ButtonLink = await $(register2ButtonSelector);
-        await usernameFieldLink.setValue(validCredentials.username);
-        await passwordFieldLink.setValue(validCredentials.password);
+        await usernameFieldLink.setValue(credsCheckUsernameField[0].username);
+        await passwordFieldLink.setValue(credsCheckUsernameField[0].password);
         await register2ButtonLink.click();
 
         const messageText = await $(messageSelector)
-        await expect(messageText).toHaveText(actualSuccessMessageText)
+        await expect(messageText).toHaveText(credsCheckUsernameField[0].expectedNotification)
 
-        await browser.pause(2000);
+        await browser.pause(500);
     });
 });
 
@@ -58,43 +92,43 @@ describe('Task2: Registration suit', () => {
     beforeEach(async () => {
         await browser.maximizeWindow();
         await browser.url(url);
-        await browser.pause(1000);
+        await browser.pause(500);
         const registerButtonLink = await $(registerButtonSelector);
         await registerButtonLink.click();
-        await browser.pause(2000);
+        await browser.pause(500);
     });
 
-    for (let i = 0; i < inValidCredentials.username.length; i++) {
-        it(`register with invalid ${inValidCredentials.username[i]}`, async () => {
+    for (let i = 1; i < credsCheckUsernameField.length; i++) {
+        it(`register with ${credsCheckUsernameField[i].checkName}`, async () => {
 
             const usernameFieldLink = await $(usernameRegisterSelector);
             const passwordFieldLink = await $(passwordRegisterSelector);
             const register2ButtonLink = await $(register2ButtonSelector);
-            await usernameFieldLink.setValue(inValidCredentials.username[i]);
-            await passwordFieldLink.setValue(validCredentials.password);
+            await usernameFieldLink.setValue(credsCheckUsernameField[i].username);
+            await passwordFieldLink.setValue(credsCheckUsernameField[i].password);
             await register2ButtonLink.click();
 
             const messageText = await $(messageSelector)
-            await expect(messageText).toHaveText(inValidCredentials.errorMessageRegisterUsername[i])
+            await expect(messageText).toHaveText(credsCheckUsernameField[i].expectedNotification)
 
-            await browser.pause(2000);
+            await browser.pause(500);
         })
     };
 
-    for (let i = 0; i < inValidCredentials.password.length; i++) {
-        it(`register with invalid ${inValidCredentials.password[i]}`, async () => {
+    for (let i = 0; i < credsCheckPasswordField.length; i++) {
+        it(`register with ${credsCheckPasswordField[i].checkName}`, async () => {
 
             const usernameFieldLink = await $(usernameRegisterSelector);
             const passwordFieldLink = await $(passwordRegisterSelector);
             const register2ButtonLink = await $(register2ButtonSelector);
-            await usernameFieldLink.setValue(validCredentials.username);
-            await passwordFieldLink.setValue(inValidCredentials.password[i]);
+            await usernameFieldLink.setValue(credsCheckPasswordField[i].username);
+            await passwordFieldLink.setValue(credsCheckPasswordField[i].password);
             await register2ButtonLink.click();
 
             const messageText = await $(messageSelector)
-            await expect(messageText).toHaveText(inValidCredentials.errorMessageRegisterPassword[i])
+            await expect(messageText).toHaveText(credsCheckPasswordField[i].expectedNotification)
 
-            await browser.pause(2000);
+            await browser.pause(500);
         })
     };
 });
@@ -107,37 +141,37 @@ describe('Task2: Login suit', () => {
         await browser.url(url);
     });
 
-    for (let i = 0; i < inValidCredentials.username.length; i++) {
-        it(`login with invalid ${inValidCredentials.username[i]}`, async () => {
+    for (let i = 1; i < credsCheckUsernameField.length; i++) {
+        it(`login with ${credsCheckUsernameField[i].checkName}`, async () => {
 
             const usernameFieldLink = await $(usernameLoginSelector);
             const passwordFieldLink = await $(passwordLoginSelector);
             const submitButtonLink = await $(submitButtonSelector);
-            await usernameFieldLink.setValue(inValidCredentials.username[i]);
-            await passwordFieldLink.setValue(validCredentials.password);
+            await usernameFieldLink.setValue(credsCheckUsernameField[i].username);
+            await passwordFieldLink.setValue(credsCheckUsernameField[i].password);
             await submitButtonLink.click();
 
             const messageText = await $(messageLoginSelector)
-            await expect(messageText).toHaveText(inValidCredentials.errorMessageLoginUsername[i])
+            await expect(messageText).toHaveText(credsCheckUsernameField[i].expectedNotificationLogin)
 
-            await browser.pause(2000);
+            await browser.pause(500);
         })
     };
 
-    for (let i = 0; i < inValidCredentials.password.length; i++) {
-        it(`register with invalid ${inValidCredentials.password[i]}`, async () => {
+    for (let i = 1; i < credsCheckPasswordField.length; i++) {
+        it(`login with ${credsCheckPasswordField[i].checkName}`, async () => {
 
             const usernameFieldLink = await $(usernameLoginSelector);
             const passwordFieldLink = await $(passwordLoginSelector);
             const submitButtonLink = await $(submitButtonSelector);
-            await usernameFieldLink.setValue(validCredentials.username);
-            await passwordFieldLink.setValue(inValidCredentials.password[i]);
+            await usernameFieldLink.setValue(credsCheckPasswordField[i].username);
+            await passwordFieldLink.setValue(credsCheckPasswordField[i].password);
             await submitButtonLink.click();
 
             const messageText = await $(messageLoginSelector)
-            await expect(messageText).toHaveText(inValidCredentials.errorMessageLoginPassword[i])
+            await expect(messageText).toHaveText(credsCheckPasswordField[i].expectedNotificationLogin)
 
-            await browser.pause(2000);
+            await browser.pause(500);
         })
     };
 });
